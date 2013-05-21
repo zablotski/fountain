af4ad667371cb14543796679b7c14e7de8228ad5
Yauheni Zablotski <333onk@gmail.com>
2013-05-21 11:36:56 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fountain::Application.config.secret_token = 'fec214a9c451998d9c0c96f3c5c2d3c85c1074c108b1aa9798dad644b2bd9d922a697feb5c6a54358c02c4e5dd882fa259a1e5458e679ad98e434019ca0306c0'
